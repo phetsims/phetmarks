6bd57c29c52507cc00213f3eb51629fc468f38b8
zepumph <michael.kauzmann@colorado.edu>
2020-07-23 11:10:06 -0800

    add deprecationWarnings flag to phetmarks, https://github.com/phetsims/states-of-matter/issues/310

--- js/phetmarks.js
+++ js/phetmarks.js
@@ -40,6 +40,7 @@
     { value: 'audioVolume=0', text: 'Mute' },
     { value: 'fuzz', text: 'Fuzz' },
     { value: 'fuzzBoard', text: 'Keyboard Fuzz' },
+    { value: 'deprecationWarnings', text: 'Deprecation Warnings' },
     { value: 'dev', text: 'Dev' },
     { value: 'profiler', text: 'Profiler' },
     { value: 'showPointers', text: 'Pointers' },
