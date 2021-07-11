69019ea96c096354da441e3b9b5bda036a89f3a1
zepumph <michael.kauzmann@colorado.edu>
2021-07-11 14:29:58 -0400

    ensure that comparing PhET-iO APIs is only done with the same randomSeed, https://github.com/phetsims/phet-io/issues/1798

--- js/phetmarks.js
+++ js/phetmarks.js
@@ -78,7 +78,7 @@
     type: 'boolean',
     text: 'Emit states to the data stream'
   }, {
-    value: 'phetioCompareAPI',
+    value: 'phetioCompareAPI&randomSeed=12345', // random seed must match that of API generation, see generatePhetioMacroAPI.js
     text: 'Compare with reference API'
   }
   ];
