9044721cf5d724bc12c0c0423ddad2183219c62b
Sam Reid <reids@colorado.edu>
2023-04-03 13:35:14 -0600

    Change query parameter shuffleListeners => listenerOrder, see https://github.com/phetsims/axon/issues/215

--- js/phetmarks.js
+++ js/phetmarks.js
@@ -60,7 +60,7 @@
     { value: 'interactiveHighlightsInitiallyEnabled', text: 'Interactive Highlights on by default' },
     { value: 'preferencesStorage', text: 'Remember previous values of preferences from localStorage.' },
     { value: 'webgl=false', text: 'No WebGL' },
-    { value: 'shuffleListeners', text: 'Shuffle listener order' },
+    { value: 'listenerOrder=random', text: 'Randomize listener order' },
     {
       value: 'locales=*', text: 'Load all locales', dependentQueryParameters: [
         { value: 'keyboardLocaleSwitcher', text: 'ctrl + u/i to cycle locales' }
