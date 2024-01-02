4438aaf684a5c6b99cf295133a5d250fcd25e5cc
phet-dev <phetadmin@gmail.com>
2024-01-02 03:18:37 -0700

    update copyright dates from daily grunt work

--- js/phetmarks.ts
+++ js/phetmarks.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2023, University of Colorado Boulder
+// Copyright 2016-2024, University of Colorado Boulder
 
 /*
  * Page for quickly launching phet-related tasks, such as simulations, automated/unit tests, or other utilities.
